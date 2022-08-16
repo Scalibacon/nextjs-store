ea6e9938f21a44d9df13b959b9782adbbbbdfcae
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-08-16 11:58:10 -0300

    add header left icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,9 @@ import HamburgerMenu from '../HamburgerMenu';
 import Image from 'next/image';
 import LogoImg from '../../assets/logo.png';
 import SearchInput from '../SearchInput';
-import { FaUserAlt } from 'react-icons/fa';
 import ProfileAccess from '../ProfileAccess';
+import { BsFillChatSquareQuoteFill, BsFillHeartFill } from 'react-icons/bs';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Header = () => {
   return(
@@ -21,7 +22,11 @@ const Header = () => {
 
         <ProfileAccess/>
 
-        <div className={styles.iconsContainer}></div>
+        <div className={styles.iconsContainer}>
+          <a><BsFillChatSquareQuoteFill size={25}/></a>
+          <a><BsFillHeartFill size={25}/></a>
+          <a><FaShoppingCart size={25}/></a>
+        </div>
       </main>
       <section className={styles.navContainer}>
         <nav></nav>
