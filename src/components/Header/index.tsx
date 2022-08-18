ea2feb9069b0434513415f95dc52bc8ee70e1e40
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-08-17 21:50:13 -0300

    dropdown menu and some more styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,14 @@ import LogoImg from '../../assets/logo.png';
 import SearchInput from '../SearchInput';
 import ProfileAccess from '../ProfileAccess';
 import { BsFillChatSquareQuoteFill, BsFillHeartFill } from 'react-icons/bs';
+import { MdOutlineArrowBackIosNew } from 'react-icons/md';
 import { FaShoppingCart } from 'react-icons/fa';
+import { useState } from 'react';
+import Category from '../../types/category';
 
 const Header = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
   return(
     <header className={styles.header}>
       <main>
@@ -30,7 +35,25 @@ const Header = () => {
         </div>
       </main>
       <section className={styles.navContainer}>
-        <nav></nav>
+        <nav>
+          <span>
+            TODAS AS CATEGORIAS <MdOutlineArrowBackIosNew size={13}/>
+            <section className={styles.dropdownMenu}>
+              <ul>
+                <li><a>Categoria 1</a></li>
+                <li><a>Categoria 2</a></li>
+                <li><a>Categoria 3</a></li>
+                <li><a>Categoria 4</a></li>
+                <li><a>Categoria 5</a></li>
+              </ul>
+            </section>
+          </span>
+          <span>LANÇAMENTOS</span>
+          <span>OFERTAS</span>
+          <span>DESTAQUES</span>
+          <span>MAIS COBIÇADOS</span>
+          <span>IMPORTADOS</span>                   
+        </nav>
       </section>
     </header>
   )
