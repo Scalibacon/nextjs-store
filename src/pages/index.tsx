301109f693d2da1809fad44356e371a2115d6f86
Matheus F Nascimento <matheusferreira_mfn@hotmail.com>
2022-08-22 13:46:37 -0300

    improvements in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { IoMdStopwatch } from "react-icons/io";
-import { BsFillLightningChargeFill } from "react-icons/bs";
+import { BsFillLightningChargeFill, BsFillStarFill } from "react-icons/bs";
+import { AiOutlineAim } from "react-icons/ai";
+import { MdKeyboardArrowRight } from "react-icons/md";
 import Header from "../components/Header";
 import SlideShow from "../components/SlideShow";
 import styles from '../styles/Home.module.scss';
 import ProductList from "../components/ProductList";
+import Image1 from '../assets/images/1.jpg';
+import Image2 from '../assets/images/2.jpg';
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -36,7 +41,55 @@ const Home: NextPage = () => {
 
         <div className={styles.row}>
           <ProductList />
-        </div>        
+        </div> 
+
+        <a className={styles.seeMore}>
+          VER TODOS
+          <MdKeyboardArrowRight size={23}/>
+        </a>  
+
+        <section className={styles.imagesContainer}>
+          <div>
+            <Image
+              src={Image1}
+              layout="responsive"
+            />
+          </div>
+
+          <div>
+            <Image
+              src={Image2}
+              layout="responsive"
+            />
+          </div>
+        </section>
+
+        <div className={styles.sectionTitle}>
+          <BsFillStarFill size={20}/>
+          <span>DESTAQUES</span>   
+          <a className={`${styles.seeMore} ${styles.insideTitle}`}>
+            VER TODOS
+            <MdKeyboardArrowRight size={23}/>
+          </a>       
+        </div>
+
+        <div className={styles.row}>
+          <ProductList />
+        </div> 
+
+        <div className={styles.sectionTitle}>
+          <AiOutlineAim size={20}/>
+          <span>MAIS PROCURADOS</span>   
+          <a className={`${styles.seeMore} ${styles.insideTitle}`}>
+            VER TODOS
+            <MdKeyboardArrowRight size={23}/>
+          </a>       
+        </div>
+
+        <div className={styles.row}>
+          <ProductList />
+        </div>
+             
       </main>
     </div>  
   )
